802fce94d3947e1949831a097c41ee9e78289d90
Moises Guilherme <moisescomp@gmail.com>
2021-01-14 23:23:30 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 //recurso (resource) é o conceito e o controlador (controller) a forma de implementar o conceito.
@@ -28,10 +27,10 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		//List é uma interface, vc prcisa insatanciar uma classe que implementa
 		//uma interface.
-		List<Category> list = service.findAll();		
+		List<CategoryDTO> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -20,9 +22,13 @@ public class CategoryService {
 	
 	//Transactional (garantir a integridade da transação)
 	//readyOnly, Evita "lock" no bando de dados, não precisa travar o banco só para ler, melhora a performace
+	
 	@Transactional(readOnly = true)   
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		// map transforma cada elemento
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 		
 	
