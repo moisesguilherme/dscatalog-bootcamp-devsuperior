db576a4f620d06834af8c7c5ed01dd4ba84b2bc8
Moises Guilherme <moisescomp@gmail.com>
2021-01-15 22:01:36 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	//RuntimeException ou Exception
@@ -8,7 +8,7 @@ public class EntityNotFoundException extends RuntimeException {
 	//            nao tratar a excessao com o try catch ou propagar para o medo que chamou
 	//RuntimeException e mais flexivel
 	
-	public EntityNotFoundException(String msg) {
+	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}	
 }
