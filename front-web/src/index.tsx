30af923d7b8245a1386d05499769b8dbea6b9fef
Moises Guilherme <moisescomp@gmail.com>
2021-02-01 11:26:37 -0300

    Add Navbar

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 // JSX -> HTML junto com JS
 // Transpilar (transformar em um js válido)
