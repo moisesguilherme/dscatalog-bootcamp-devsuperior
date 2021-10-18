184021c5e46c2c10338d85bd66e6a7d59fce2590
Moises Guilherme <moisescomp@gmail.com>
2021-10-18 12:06:21 -0300

    Layout Login Page

---
+++ front-web/src/pages/Auth/components/Card/index.tsx
@@ -0,0 +1,23 @@
+import { totalmem } from 'os';
+import React from 'react';
+import './styles.scss';
+
+
+type Props = {
+    title: String;
+    children: React.ReactNode;
+}
+
+const AuthCard = ({title, children}: Props) => {
+    
+    return( 
+        <div className="card-base auth-card">
+            <h1 className="auth-card-title">
+                {title}
+            </h1>
+                {children}
+        </div>
+    )
+}
+
+export default AuthCard;
---
+++ front-web/src/pages/Auth/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { ReactComponent as AuthImage } from 'core/assets/images/auth.svg';
+import Login from './components/Login';
+
+import './styles.scss';
+
+const Auth = () => (
+    <div className="auth-container">
+        <div className="auth-info">
+            <h1 className="auth-info-title">
+                Divulgue seus produtos <br /> no DS Catalog
+            </h1>
+
+            <p className="auth-info-subtitle">
+                Faça parte do nosso catálogo de divulgação e <br /> aumente a venda dos seus produtos.
+            </p>
+            <AuthImage />
+        </div>
+        <div className="auth-content">
+            <Switch>
+                <Route path="/admin/auth/login">
+                    <Login/>
+                </Route>
+                <Route path="/admin/auth/register">
+                    <h1>Cadastro</h1>
+                </Route>
+                <Route path="/admin/auth/recover">
+                    <h1>Recuperar</h1>
+                </Route>
+            </Switch>
+        </div>
+
+    </div>
+);
+
+export default Auth;
