560501095c86cfb52530ad2799c50ccd26ff3611
Moises Guilherme <moisescomp@gmail.com>
2021-10-19 11:30:13 -0300

    Make login and save user session data

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,33 +1,51 @@
-import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
+
+import ButtonIcon from 'core/components/ButtonIcon';
 import './styles.scss';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError ] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
-        console.log(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false);
+                saveSessionData(response.data);
+                history.push('/admin');
+            })
+            .catch(() => {
+                setHasError(true);
+            })
     }
 
     return( 
         <AuthCard title="login">
+          {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos
+                </div>    
+          )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
-                    {...register("email")} 
+                    {...register("username", { required: true })} 
                     type="email" 
                     className="form-control input-base margin-bottom-30" 
                     placeholder="Email"
                 />
                 <input 
-                    {...register("password")} 
+                    {...register("password", { required: true, minLength: 5})} 
                     type="password" 
                     className="form-control input-base" 
                     placeholder="Senha"
