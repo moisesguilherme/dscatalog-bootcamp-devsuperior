9c2fa080308b4d282007e473bd892bd14147c2ec
Moises Guilherme <moisescomp@gmail.com>
2021-10-23 18:45:16 -0300

    Integrated product listing with backend

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,9 +1,31 @@
-import React from  'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { ProductsResponse } from 'core/types/product';
+import { makePrivateRequest } from 'core/utils/request';
+import Pagination from 'core/components/Pagination';
+
 import Card from '../Card';
 
-const List = () => {
+const List = () => {    
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+
+        setIsLoading(true);
+        makePrivateRequest({ url: '/products', params})
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);                
+            })
+    }, [activePage]);
+    
 
     const handleCreate = () => {
         history.push('/admin/products/create');
@@ -16,9 +38,18 @@ const List = () => {
             </button>
 
             <div className="admin-list-container">
-                <Card  />
-                <Card />
-                <Card />
+            {productsResponse?.content.map(product => (
+                    <Card key={product.id} product={product}  />
+                ))}                
+                {productsResponse && (
+                    <Pagination 
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
+           
+
             </div>
         </div>
     )
