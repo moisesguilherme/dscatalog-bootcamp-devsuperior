f89f6056ae9b58d0c12a34ca5e08837755b5d933
Moises Guilherme <moisescomp@gmail.com>
2021-12-25 13:08:53 -0300

    Fix get all users

--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -37,7 +37,7 @@ public class UserResource {
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "fristName") String orderBy
+			@RequestParam(value = "orderBy", defaultValue = "firstName") String orderBy
 			) {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
